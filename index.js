65052d3aa8a90162cedf897fa3968f4ebad3b55d
Spencer <eklund.spencer@outlook.com>
2023-07-02 10:38:09 -0600

    Created different functions to modify arrats

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (name) {
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+const newCats = [...cats, "Broom"];
+return newCats
+}
+function prependCat(name) {
+    const newCatsTwo = ["Arnold",...cats];
+    return newCatsTwo
+}
+function removeLastCat() {
+const newCatsThree = cats.slice(0,2)
+return newCatsThree
+}
+function removeFirstCat() {
+const newCatsFour = cats.slice(1)
+return newCatsFour 
+}
